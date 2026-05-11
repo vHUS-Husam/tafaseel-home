54d8a8b8062813fa0aed51ac1555575d68df9647
Husam <husambadawi213@gmail.com>
2026-05-11 18:57:56 +0300

    Fix API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tafasel-home-ps.onrender.com/api',
+  baseURL: 'https://tafaseel-home.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
